fa7b8117f9f187a61d2941d3c4536982158e9942
Marcus Vivar <marcospalominovivar@gmail.com>
2023-08-03 17:58:42 +0200

    bug: userprofile upload photo

--- src/features/courses/components/CoursesCardHome.jsx
+++ src/features/courses/components/CoursesCardHome.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import { FiUser } from "react-icons/fi";
 import { Link } from 'react-router-dom';
 
-export const CoursesCardHome = ({course}) => {
+export const CoursesCardHome = ({ course }) => {
     return (
         <>
             <Link to={`/app/courses/${course.id}`}>
-                <div className="max-w-sm bg-white  rounded-lg shadow cursor-pointer h-[35rem] shadow2">
+                <div className="max-w-sm bg-white rounded-lg shadow cursor-pointer shadow2">
                     <img className="rounded-t-lg w-full h-[13rem] object-fill" src={course.cover.url} alt="" />
-                    <div className="p-3 flex flex-col justify-center items-center">
-                        <h3 className='text-center'>{course.title}</h3>
-                        <p className='text-xs  font-normal text-gray-700'>{course.course_type}</p>
+                    <div className="p-3 flex flex-col justify-between h-full">
+                        <h3 className='text-center truncate w-full overflow-hidden'>{course.title}</h3>
+                        <p className='text-xs font-normal text-gray-700'>{course.course_type}</p>
                         <div className='container bg-gray-100 py-1.5 my-6 rounded '>
                         </div>
                         <div className='container flex flex-row space-x-20 justify-center'>
-
                             <div className=' px-2 bg-gray-100 h-[3rem] flex justify-center text-center align-middle space-x-1 rounded items-center'>
                                 <FiUser size={19} className='my-1 justify-center text-center align-middle' />
                                 <p className=' text-base font-normal'>{course.students.length}</p>
@@ -24,8 +23,8 @@ export const CoursesCardHome = ({course}) => {
                                 <p className='text-base font-normal'>{course.professor['name']}</p>
                             </div>
                         </div>
-                        <div className='container bg-gray-100 rounded my-8 h-auto'>
-                            <p className='text-sm font-normal px-5 py-5 text-ellipsis overflow-hidden  '>{course.description}</p>
+                        <div className='bg-gray-100 rounded my-8 overflow-hidden'>
+                            <p className='text-sm font-normal px-5 py-5'>{course.description}</p>
                         </div>
                     </div>
                 </div>
