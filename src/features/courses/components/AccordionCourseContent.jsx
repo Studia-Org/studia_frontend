3f604f17f2560509967925b6c654ed55d19dfa81
Marcus Vivar <marcospalominovivar@gmail.com>
2023-08-21 12:58:44 +0200

    refactor: animations

--- src/features/courses/components/AccordionCourseContent.jsx
+++ src/features/courses/components/AccordionCourseContent.jsx
@@ -40,26 +40,29 @@ export const AccordionCourseContent = ({ courseContentInformation, setCourseSubs
     function RenderCourseInsideSectionContent(subsection, titulo) {
         if (new Date(subsection.attributes.start_date).toISOString() > new Date().toISOString()) {
             return (
-                <div className='flex cursor-pointer my-1 hover:border-l-4 items-center py-3 '>
-                    <p className='text-base font-normal ml-4 '>
-                        <span>🔒 </span>
-                    </p>
-                    <span className='truncate w-2/4 ml-3'>{subsection.attributes.title}</span>
+                <div className='flex cursor-pointer my-1  items-center'>
+                    <div className=' hover:translate-x-2 duration-150 py-3 flex'>
+                        <p className='text-base font-normal ml-4 '>
+                            <span>🔒 </span>
+                        </p>
+                        <span className='truncate w-3/4 ml-3'>{subsection.attributes.title}</span>
+                    </div>
                     {selectFaseSectionContent(subsection.attributes.fase)}
                 </div>
             )
         }
         return (
-            <div className='flex cursor-pointer my-1 hover:border-l-4 items-center py-3 ' onClick={() => handleSections(titulo, subsection.attributes)}>
-                <p className='text-base font-normal ml-4   '>
-                    {subsection.attributes.finished === "False" ? (
-                        <span role="img" aria-label="circle">⭕ </span>
-                    ) : (
-                        <span role="img" aria-label="checkmark">✅ </span>
-                    )}
-
-                </p>
-                <span className='truncate w-2/4 ml-3'>{subsection.attributes.title}</span>
+            <div className='flex cursor-pointer my-1 items-center' onClick={() => handleSections(titulo, subsection.attributes)}>
+                <div className=' hover:translate-x-2 duration-150 py-3 flex '>
+                    <p className='text-base font-normal ml-4   '>
+                        {subsection.attributes.finished === "False" ? (
+                            <span role="img" aria-label="circle">⭕ </span>
+                        ) : (
+                            <span role="img" aria-label="checkmark">✅ </span>
+                        )}
+                    </p>
+                    <span className='truncate w-3/4 ml-3'>{subsection.attributes.title}</span>
+                </div>
                 {selectFaseSectionContent(subsection.attributes.fase)}
             </div>
         )
@@ -91,7 +94,7 @@ export const AccordionCourseContent = ({ courseContentInformation, setCourseSubs
     return (
         <div className='flex-shrink-0 w-full sm:w-auto'>
             <div className='mt-8 bg-white rounded-lg  px-5 py-5  sm:mr-9 sm:right-0 sm:w-[30rem] w-full shadow-md sm:visible collapse'>
-                <p className='text-xl font-medium'>Course content</p>
+                <p className='text-xl font-semibold'>Course content</p>
                 <hr className="h-px my-8 bg-gray-400 border-0"></hr>
                 {courseContentInformation.map(RenderCourseContent)}
             </div>
