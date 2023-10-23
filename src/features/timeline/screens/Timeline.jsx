73414a18ac4c1012a84128e2b98a70e466bc1d5f
rogeranyor <rogeranyor@gmail.com>
2023-10-23 16:00:51 +0200

    fix min width

--- src/features/timeline/screens/Timeline.jsx
+++ src/features/timeline/screens/Timeline.jsx
@@ -10,10 +10,10 @@ const Timeline = () => {
         <div className='max-h-full  bg-white '>
             <Navbar />
             <Sidebar section={'timeline'} />
-            <div className='flex min-h-[calc(100vh-8rem)] sm:ml-80 sm:min-w-[calc(100vw-16rem)] sm:flex-nowrap bg-white '>
+            <div className='flex min-h-[calc(100vh-8rem)] sm:ml-80 sm:min-w-[calc(100vw-20rem)] sm:flex-nowrap bg-white '>
                 <div className='rounded-tl-3xl bg-[#e7eaf886] max-w-full max-h-full'>
                     <div className='pt-9 pl-12 h-full font-bold text-2xl'>
-                        <div className='bg-[#f7f7f7] p-4 pb-0 h-full rounded-tl-2xl  shadow-lg'>
+                        <div className='bg-[#f7f7f7] p-4 pb-0 h-full rounded-tl-2xl  shadow-lg max-w-[98%]'>
                             <TimelineComponent />
                         </div>
                     </div>
