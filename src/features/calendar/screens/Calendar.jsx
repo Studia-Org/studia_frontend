e231dd4e8a8ba84469904f330e4fe09fad0a3cc9
rogeranyor <rogeranyor@gmail.com>
2023-10-25 18:29:12 +0200

    calendar css for responsiveness and now you can click an event to watch the info

--- src/features/calendar/screens/Calendar.jsx
+++ src/features/calendar/screens/Calendar.jsx
@@ -8,13 +8,17 @@ import { API } from "../../../constant";
 import { message } from "antd";
 import { getToken } from "../../../helpers";
 import { FiPlus } from 'react-icons/fi';
+import './calendar.css'
 
 const CalendarEvents = () => {
     const { user } = useAuthContext();
     const [open, setOpen] = useState(false);
+    const [infoModal, setInfoModal] = useState(false);
+    const [infoModalData, setInfoModalData] = useState({});
     const [title, setTitle] = useState('');
     const [date, setDate] = useState('');
     const [events, setEventList] = useState([]);
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
     const handleTitleChange = (value) => {
         setTitle(value);
@@ -101,6 +105,7 @@ const CalendarEvents = () => {
                         placement="top"
                         trigger="click"
                         speaker={
+
                             <Popover>
                                 {list.map((item, index) => (
                                     <p className='' key={index}>
@@ -115,15 +120,19 @@ const CalendarEvents = () => {
                 </li>
             );
             return (
-                <ul className="calendar-todo-list">
+                <ul className="calendar-todo-list over overflow-hidden">
 
                     {displayList.map((item, index) => (
 
-                        <li className='text-left' key={index}>
-                            <Badge /> <strong>{formatTime(item.date)}</strong>  - {item.title}
+                        <li onClick={() => { setInfoModal(true); setInfoModalData(list) }} className={`text-left ${innerWidth < 690 ? "text-center" : ""}`} key={index}>
+                            {innerWidth < 690 ? <Badge /> :
+                                <> <Badge />
+                                    <b>{formatTime(item.date)}</b> - {item.title}
+                                </>}
+
                         </li>
                     ))}
-                    {moreCount ? moreItem : null}
+                    {moreCount && innerWidth > 690 ? moreItem : null}
                 </ul>
             );
         }
@@ -131,6 +140,9 @@ const CalendarEvents = () => {
         return null;
     }
 
+    window.addEventListener('resize', () => {
+        setInnerWidth(window.innerWidth);
+    });
     function formatTime(dateTimeString) {
         const options = {
             hour: 'numeric',
@@ -140,55 +152,88 @@ const CalendarEvents = () => {
         const date = new Date(dateTimeString);
         return date.toLocaleTimeString(undefined, options);
     }
+    console.log(infoModalData)
     return (
-        <div className='flex-1 min-h-full max-w-full w-full rounded-tl-3xl bg-[#e7eaf886] '>
-            <div className='p-9 lg:px-12 min-h-full px-5 font-bold text-2xl overflow-y-auto'>
-                <div className='min-h-full bg-white rounded-xl lg:p-5 mt-3 shadow-lg '>
-                    <div className='flex'>
-                        <Modal size='sm' open={open} onClose={handleClose}>
-                            <Modal.Header>
-                                <Modal.Title>Add Event</Modal.Title>
-                            </Modal.Header>
-                            <Modal.Body>
-                                <Form.Group >
-                                    <Form.ControlLabel>Title</Form.ControlLabel>
-                                    <Input value={title} onChange={handleTitleChange} />
-                                </Form.Group >
-                                <Form.Group >
-                                    <Form.ControlLabel>Date</Form.ControlLabel>
-                                    <Input type='datetime-local' value={date} onChange={handleDateChange} />
-                                </Form.Group >
-                            </Modal.Body>
-                            <Modal.Footer>
-                                <Button onClick={handleClose} appearance="subtle">
-                                    Cancel
-                                </Button>
-                                <Button onClick={handleAddEvent} appearance="primary">
-                                    Add Event
-                                </Button>
-                            </Modal.Footer>
-                        </Modal>
-                    </div>
+        <>
+            <div className='max-w-full rounded-tl-3xl bg-[#e7eaf886] '>
+                <div className='font-bold text-2xl overflow-y-auto h-full'>
 
-                    <div className='font-normal '>
-                        <Calendar compact={window.innerWidth < 690} bordered renderCell={renderCell} />
+                    <div className='grid h-full '>
+                        <div className='font-normal max-w-[95%] max-h-[95%] overflow-hidden bg-white rounded-xl my-auto mx-auto '>
+                            <Calendar compact={innerWidth < 690} bordered renderCell={renderCell} />
+                        </div>
                     </div>
-
+                </div>
+                <div className='fixed right-10 bottom-10'>
+                    <button
+                        type="button"
+                        data-dial-toggle="speed-dial-menu-dropdown"
+                        aria-controls="speed-dial-menu-dropdown"
+                        className="flex shadow-lg items-center transition  justify-center ml-auto text-white bg-blue-600 rounded-full w-14 h-14 hover:bg-blue-600 hover-scale active-scale  "
+                        onClick={handleOpen}
+                    >
+                        <FiPlus size={26} />
+                        <span className="sr-only"></span>
+                    </button>
                 </div>
             </div>
-            <div className='fixed right-10 bottom-10'>
-                <button
-                    type="button"
-                    data-dial-toggle="speed-dial-menu-dropdown"
-                    aria-controls="speed-dial-menu-dropdown"
-                    className="flex shadow-lg items-center transition  justify-center ml-auto text-white bg-blue-600 rounded-full w-14 h-14 hover:bg-blue-600 hover-scale active-scale  "
-                    onClick={handleOpen}
-                >
-                    <FiPlus size={26} />
-                    <span className="sr-only"></span>
-                </button>
+            <div className='flex shadow-lg'>
+                <Modal size='sm' open={open} onClose={handleClose}>
+                    <Modal.Header>
+                        <Modal.Title>Add Event</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form.Group >
+                            <Form.ControlLabel>Title</Form.ControlLabel>
+                            <Input value={title} onChange={handleTitleChange} />
+                        </Form.Group >
+                        <Form.Group >
+                            <Form.ControlLabel>Date</Form.ControlLabel>
+                            <Input type='datetime-local' value={date} onChange={handleDateChange} />
+                        </Form.Group >
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={handleClose} appearance="subtle">
+                            Cancel
+                        </Button>
+                        <Button onClick={handleAddEvent} appearance="primary">
+                            Add Event
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
             </div>
-        </div>
+            <div className=' shadow-lg'>
+                <Modal size='xs' open={infoModal} onClose={() => setInfoModal(false)}>
+                    <Modal.Header>
+                        <Modal.Title>
+                            {infoModalData && infoModalData.length > 0 && (
+                                <div>
+                                    <b>{new Date(infoModalData[0].date).toLocaleDateString()}</b>
+                                </div>
+                            )}
+                        </Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {infoModalData && (
+                            Object.keys(infoModalData).map((item, index) => (
+                                <div key={index}>
+                                    <b> {infoModalData[item].title}</b> - {new Date(infoModalData[item].date).toLocaleTimeString()}
+                                </div>
+                            ))
+                        )}
+
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button onClick={() => setInfoModal(false)} appearance="subtle">
+                            Close
+                        </Button>
+                        <Button onClick={() => { setInfoModal(false); setOpen(true) }} appearance="primary">
+                            Add Event
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </div>
+        </>
 
     )
 }
--- src/features/timeline/screens/Timeline.jsx
+++ src/features/timeline/screens/Timeline.jsx
@@ -7,10 +7,9 @@ import "typeface-roboto";
 
 const Timeline = () => {
     return (
-
-        <div className='rounded-tl-3xl bg-[#e7eaf886] max-w-full max-h-full'>
-            <div className='pt-9 pl-12 h-full font-bold text-2xl'>
-                <div className='bg-[#f7f7f7] p-4 pb-0 h-full rounded-tl-2xl  shadow-lg max-w-[98%]'>
+        <div className='rounded-tl-3xl bg-[#e7eaf886] '>
+            <div className='pt-9 pl-12 font-bold text-2xl max-w-[calc(100%-5.25rem)] '>
+                <div className='bg-[#f7f7f7] p-4 pb-0  rounded-tl-2xl  shadow-lg '>
                     <TimelineComponent />
                 </div>
             </div>
