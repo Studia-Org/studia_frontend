352f24fd5be916dab01f135a6299243b059a269a
rogeranyor <rogeranyor@gmail.com>
2023-10-25 18:57:57 +0200

    time line responsive for bigger screens and hover move on timeline

--- src/features/timeline/screens/Timeline.jsx
+++ src/features/timeline/screens/Timeline.jsx
@@ -8,8 +8,8 @@ import "typeface-roboto";
 const Timeline = () => {
     return (
         <div className='rounded-tl-3xl bg-[#e7eaf886] '>
-            <div className='pt-9 pl-12 font-bold text-2xl max-w-[calc(100%-5.25rem)] '>
-                <div className='bg-[#f7f7f7] p-4 pb-0  rounded-tl-2xl  shadow-lg '>
+            <div className='pt-9 pl-12 font-bold text-2xl max-w-[calc(100%-5.25rem)] h-[95%] '>
+                <div className='bg-[#f7f7f7] p-4 pb-0  rounded-tl-2xl  h-full shadow-lg '>
                     <TimelineComponent />
                 </div>
             </div>
