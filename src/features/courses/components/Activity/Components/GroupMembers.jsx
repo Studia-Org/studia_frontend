d827b73a5c73d8c2ded23dfdf64ff7a730c6b84d
Topiksit0 <marcospalominovivar@gmail.com>
2024-03-20 12:51:24 +0100

    Update GroupMembers.jsx

--- src/features/courses/components/Activity/Components/GroupMembers.jsx
+++ src/features/courses/components/Activity/Components/GroupMembers.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Avatar } from 'rsuite';
 import { MoonLoader } from 'react-spinners';
+import { Link } from 'react-router-dom';
 
 function GroupMembers({ activityGroup, loadingGroup, small = false }) {
     const sizeAvatar = small ? 'md' : 'large'
@@ -16,21 +17,20 @@ function GroupMembers({ activityGroup, loadingGroup, small = false }) {
                 <div className={`flex ${small ? "flex gap-x-2" : "flex-col px-5"}  my-2`}>
                     {activityGroup.users.data.map((user, index) => {
                         return (
-                            <a key={index}
-                                href={`/app/profile/${user.id}`}
-                                rel='noreferrer'
-                                aria-label={`Go to ${user.attributes.username} profile`}
-                                title={`Go to ${user.attributes.username} profile`}
-                                target='_blank'
-                                className="flex items-center gap-2 px-3 py-3 mt-3 duration-150 bg-white rounded-md shadow-md cursor-pointer hover:bg-gray-50">
-                                <Avatar size={sizeAvatar} src={user.attributes.profile_photo.data?.attributes?.url} />
-                                {
-                                    !small ?
-                                        <p className="text-sm text-gray-800">{user.attributes.name} · <span className='font-medium text-black'>{user.attributes.username}</span> </p>
-                                        :
-                                        null
-                                }
-                            </a>
+                            <Link
+                            key={index}
+                            to={`/app/profile/${user.id}`}
+                            aria-label={`Go to ${user.attributes.username} profile`}
+                            title={`Go to ${user.attributes.username} profile`}
+                            className="flex items-center gap-2 px-3 py-3 mt-3 duration-150 bg-white rounded-md shadow-md cursor-pointer hover:bg-gray-50"
+                          >
+                            <Avatar size={sizeAvatar} src={user.attributes.profile_photo.data?.attributes?.url} />
+                            {!small ? (
+                              <p className="text-sm text-gray-800">
+                                {user.attributes.name} · <span className="font-medium text-black">{user.attributes.username}</span>{' '}
+                              </p>
+                            ) : null}
+                          </Link>
                         )
                     })}
                 </div>
