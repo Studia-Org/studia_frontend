23e46d116647819bb2409dacc40e7e3c740ab23a
rogeranyor <rogeranyor@gmail.com>
2024-03-21 18:02:30 +0100

    fix responsiveness for list of subsections when creating course

--- src/features/courses/components/CreateCourses/CourseSections/CreateCourseSubsectionsList.jsx
+++ src/features/courses/components/CreateCourses/CourseSections/CreateCourseSubsectionsList.jsx
@@ -11,7 +11,6 @@ export const CreateCourseSubsectionsList = ({
     setEditSubsectionFlag,
     setSubsectionEditing,
 }) => {
-
     const deleteSubsection = () => {
         setCreateCourseSectionsList((prevSections) =>
             prevSections.map((section) =>
@@ -60,11 +59,8 @@ export const CreateCourseSubsectionsList = ({
                 setSubsectionEditing(subsection);
             }}
             style={style}
-            className="cursor-pointer mb-10 ml-8 mt-8 flex items-center h-[2rem] relative mr-5"
+            className="cursor-pointer mb-10 ml-8 mt-8 flex items-center min-h-[2rem] relative mr-5"
         >
-            {/* Background bar */}
-            <div className="absolute w-[calc(100%+4rem)] bg-gray-50 h-[3rem] -left-14 rounded-md border"></div>
-
             {/* Phase indicator */}
             <span
                 {...attributes}
@@ -85,45 +81,48 @@ export const CreateCourseSubsectionsList = ({
                     />
                 </svg>
             </span>
+            {/* Background bar */}
+            <div className="flex items-center gap-x-3 justify-between w-[calc(100%+4rem)] bg-gray-50 min-h-[3rem] -left-14 rounded-md border">
 
-            <div className="relative flex flex-row items-center gap-2">
-                <div className="flex flex-col justify-center ml-5">
-                    <p className="">{subsection.title}</p>
+                <div className="flex items-center gap-2 ">
+                    <div className="flex flex-col justify-center ml-5">
+                        <p className="">{subsection.title}</p>
+                    </div>
+                    <CheckSubsectionErrors subsection={subsection} />
                 </div>
-                <CheckSubsectionErrors subsection={subsection} />
-            </div>
 
-            <Popconfirm
-                title="Delete the subsection"
-                description="Are you sure to delete this subsection?"
-                okText="Yes"
-                okType="danger"
-                onConfirm={(e) => {
-                    e.stopPropagation();
-                    deleteSubsection();
-                    setEditSubsectionFlag(false);
-                    message.success('Subection deleted successfully');
-                }}
-                onCancel={(e) => {
-                    e.stopPropagation();
-                }}
-                cancelText="No"
-            >
-                <Button
-                    className="absolute right-0"
-                    onClick={(event) => event.stopPropagation()}
-                    icon={
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="w-5 h-5 mt-[1px]"
-                        >
-                            <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clipRule="evenodd" />
-                        </svg>
-                    }
-                ></Button>
-            </Popconfirm>
+                <Popconfirm
+                    title="Delete the subsection"
+                    description="Are you sure to delete this subsection?"
+                    okText="Yes"
+                    okType="danger"
+                    onConfirm={(e) => {
+                        e.stopPropagation();
+                        deleteSubsection();
+                        setEditSubsectionFlag(false);
+                        message.success('Subection deleted successfully');
+                    }}
+                    onCancel={(e) => {
+                        e.stopPropagation();
+                    }}
+                    cancelText="No"
+                >
+                    <Button
+                        className=" !mr-2"
+                        onClick={(event) => event.stopPropagation()}
+                        icon={
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 20 20"
+                                fill="currentColor"
+                                className="w-5  h-5 mt-[1px]"
+                            >
+                                <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8.28 7.22a.75.75 0 00-1.06 1.06L8.94 10l-1.72 1.72a.75.75 0 101.06 1.06L10 11.06l1.72 1.72a.75.75 0 101.06-1.06L11.06 10l1.72-1.72a.75.75 0 00-1.06-1.06L10 8.94 8.28 7.22z" clipRule="evenodd" />
+                            </svg>
+                        }
+                    ></Button>
+                </Popconfirm>
+            </div>
         </li>
     );
 };
