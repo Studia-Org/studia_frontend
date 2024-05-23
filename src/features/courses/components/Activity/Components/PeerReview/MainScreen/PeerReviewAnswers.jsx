620247de4e92138dfdd4d80b8dc6b6cadf726037
rogeranyor <rogeranyor@gmail.com>
2024-05-23 19:12:58 +0200

    fix peerreview answers background when overflow

--- src/features/courses/components/Activity/Components/PeerReview/MainScreen/PeerReviewAnswers.jsx
+++ src/features/courses/components/Activity/Components/PeerReview/MainScreen/PeerReviewAnswers.jsx
@@ -4,30 +4,26 @@ function PeerReviewAnswers({ answers, data }) {
     const number = !isNaN(parseFloat(data[0].split("-")[0])) && isFinite(data[0].split("-")[0])
     return (
         answers !== undefined && answers !== null ?
-            <section className=" h-full max-h-[600px]">
-                <div className="grid font-semibold h-[calc(100%-4rem)] max-h-[600px]
-                     grid-cols-2 shadow-md rounded-md  ">
-
-                    {Object.keys(answers).map((criterion, index) => {
-                        const mark = Object.keys(answers[criterion])[0];
-                        const upperLimit = data[data.length - 1].split("-")[1] || data[data.length - 1];
-                        const feedback = answers[criterion][mark];
-                        const isFirstRow = index === 0;
-                        const isLastRow = index === Object.keys(answers).length - 1;
-                        const cornerClasses = isFirstRow ? "rounded-tl-md " : (isLastRow ? " " : "");
+            <section className="grid font-semibold h-[calc(100%-4rem)] grid-cols-2  rounded-md  ">
+                {Object.keys(answers).map((criterion, index) => {
+                    const mark = Object.keys(answers[criterion])[0];
+                    const upperLimit = data[data.length - 1].split("-")[1] || data[data.length - 1];
+                    const feedback = answers[criterion][mark];
+                    const isFirstRow = index === 0;
+                    const isLastRow = index === Object.keys(answers).length - 1;
+                    const cornerClasses = isFirstRow ? "rounded-tl-md " : (isLastRow ? " " : "");
 
 
-                        return (
-                            <>
-                                <div className={`flex items-center p-4 break-allbg-purple-300 text-gray-900 bg-[#ced6ef]  ${cornerClasses}`}>{criterion}</div>
-                                <div className={`${isFirstRow ? "rounded-tr-md" : ""} break-all bg-[#ced6ef] 
+                    return (
+                        <>
+                            <div className={`flex items-center p-4 break-allbg-purple-300 text-gray-900 bg-[#ced6ef]  ${cornerClasses}`}>{criterion}</div>
+                            <div className={`${isFirstRow ? "rounded-tr-md" : ""} break-all bg-[#ced6ef] 
                                                 flex items-center p-4  text-gray-900`}>{mark + (number ? "/" + upperLimit : "")}</div>
-                                <div className={`col-span-2 break-all flex items-center p-4 h-full bg-white 
+                            <div className={`col-span-2 break-all flex items-center p-4 h-full bg-white 
                                                 ${isLastRow ? "rounded-bl-md  rounded-br-md" : ""}`}>{feedback}</div>
-                            </>
-                        );
-                    })}
-                </div>
+                        </>
+                    );
+                })}
             </section>
             :
             <section className="p-10 pt-0 ">
