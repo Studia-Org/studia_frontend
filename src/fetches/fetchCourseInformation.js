6fc82dc8cba3e0bc914b25717e3a10345b3fb7d8
Marcus Vivar <67715243+Topiksit0@users.noreply.github.com>
2024-05-28 18:59:42 +0200

    admin replicate courses

--- src/fetches/fetchCourseInformation.js
+++ src/fetches/fetchCourseInformation.js
@@ -3,12 +3,13 @@ export async function fetchCourseInformation({ courseId }) {
   try {
     const response = await fetch(
       `${API}/courses/${courseId}?populate=sections.subsections.activity,sections.subsections.paragraphs,students.profile_photo,students.qualifications.activity,students.qualifications.file,professor.profile_photo,sections.subsections.landscape_photo,sections.subsections.questionnaire,` +
-      `students.groups.qualifications.file,students.groups.qualifications.activity, students.groups.users,students.groups.users.profile_photo, students.groups.activity`);
+      `students.groups.qualifications.file,students.groups.qualifications.activity, students.groups.users,students.groups.users.profile_photo,students.groups.activity,cover`);
     const data = await response.json();
     return {
       courseInformation: data?.data?.attributes?.sections?.data ?? [],
       students: data?.data?.attributes?.students ?? [],
       professor: data?.data?.attributes?.professor?.data,
+      allData: data?.data?.attributes,
     };
   } catch (error) {
     console.error(error);
