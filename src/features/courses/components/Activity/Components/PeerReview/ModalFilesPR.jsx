c8368960a09542d27639ea58a25bddb454f1137b
rogeranyor <rogeranyor@gmail.com>
2024-06-03 18:16:29 +0200

    cut text in files

--- src/features/courses/components/Activity/Components/PeerReview/ModalFilesPR.jsx
+++ src/features/courses/components/Activity/Components/PeerReview/ModalFilesPR.jsx
@@ -24,11 +24,11 @@ export const ModalFilesPR = ({ files, activityTitle, isModalOpen, setIsModalOpen
 
     function renderFiles(file) {
         return (
-            <Button onClick={() => downloadFile(file.attributes)} className='flex items-center rounded-md border p-7 w-full'>
+            <Button onClick={() => downloadFile(file.attributes)} className='flex items-center w-full border rounded-md p-7 '>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-5">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                 </svg>
-                <p>{file.attributes.name}</p>
+                <p className='max-w-[100%] text-ellipsis overflow-x-hidden'>{file.attributes.name}</p>
             </Button>
         )
     }
@@ -39,12 +39,12 @@ export const ModalFilesPR = ({ files, activityTitle, isModalOpen, setIsModalOpen
                     OK
                 </Button>
             ]}>
-            <p className='text-gray-700 text-sm'>Files delivered by {studentName} on activity {activityTitle}</p>
+            <p className='text-sm text-gray-700'>Files delivered by {studentName} on activity {activityTitle}</p>
             {
                 (files?.length === 0 || !files) &&
                 <Empty className='mt-7' description='There are no files delivered' />
             }
-            <div className='space-y-5 mt-3'>
+            <div className='mt-3 space-y-5'>
                 {files?.map(renderFiles)}
             </div>
         </Modal>
