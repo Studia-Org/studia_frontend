80e6ea7f0dc6c0edaa165ae7484dcb9764bc9557
rogeranyor <rogeranyor@gmail.com>
2024-07-03 16:45:33 +0200

    translate navbar

--- src/shared/elements/Navbar/Navbar.jsx
+++ src/shared/elements/Navbar/Navbar.jsx
@@ -15,22 +15,19 @@ export const Navbar = () => {
             <Link to={'/app/courses'}>
                 <h1 className='pr-3 ml-16 text-3xl italic font-bold leading-none tracking-tight cursor-pointer '>Uptitude<span className='text-4xl text-pink-500 '>.</span></h1>
             </Link>
-
             <div className='flex items-center justify-end pr-2 md:pr-5 gap-x-6'>
                 <ReportBug />
                 <Notifications />
                 <Tag className={'hidden lg:block'} User={user} />
                 {user && <p className='hidden font-medium lg:block'>{user['name']}</p>}
                 <button onClick={() => navigate(`/app/profile/${user.id}/`)} className='rounded '>
-                    {user && user['profile_photo'] ? (
+                    {user && user['profile_photo'] &&
                         <img
                             src={user['profile_photo'].url}
                             className='object-cover rounded-lg cursor-pointer h-14 w-14'
                             alt="user profile "
                         />
-                    ) : (
-                        null
-                    )}
+                    }
                 </button>
             </div>
         </nav>
