cee3cc9b770a97d71bfa1dedfc6bfddee3b69724
Marcos Vivar <marcospalominovivar@gmail.com>
2024-07-30 10:13:28 +0200

    Update RecommendationImprovement.jsx

--- src/features/courses/components/CoursesInside/Questionnaire/RecommendationImprovement.jsx
+++ src/features/courses/components/CoursesInside/Questionnaire/RecommendationImprovement.jsx
@@ -1,22 +1,18 @@
-import { Modal } from 'antd'
+import { Empty, List, Modal, Typography } from 'antd'
 import React, { useEffect, useState } from 'react'
 
 
-const ImprovementCard = ({ text, color}) => {
-    return (
-        <div className={`flex items-center p-2 my-3  border rounded-md ${color}`}>
-            <p className='mr-10'>{text}</p>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute w-5 h-5 right-10">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
-            </svg>
-        </div>
-    )
-}
-
 export const RecommendationImprovement = ({ isModalOpen, setIsModalOpen, checkImprovement }) => {
-    console.log(checkImprovement)
     const [improved, setImproved] = useState([]);
     const [worsen, setStillToImprove] = useState([]);
+
+    let localeWorsen = {
+        emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="Congratulations! You haven't worsened anything. Keep up the great work!" />,
+    };
+
+    let localeImprove = {
+        emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="You haven't improved anything. Keep working!" />,
+    };
 
     const handleCancel = () => {
         setIsModalOpen(false);
@@ -33,7 +29,7 @@ export const RecommendationImprovement = ({ isModalOpen, setIsModalOpen, checkIm
                 }
             });
             checkImprovement.current.forEach(recommendation => {
-                if(!checkImprovement.previous.includes(recommendation)){
+                if (!checkImprovement.previous.includes(recommendation)) {
                     worsen.add(recommendation);
                 }
             });
@@ -45,17 +41,35 @@ export const RecommendationImprovement = ({ isModalOpen, setIsModalOpen, checkIm
     return (
         <Modal title="Improvements" open={isModalOpen} onCancel={handleCancel} footer={null} width={"60rem"}>
             <p>What did you improve: </p>
-            {
-                improved && improved.map((text, index) => (
-                    <ImprovementCard key={index} text={text} color={"bg-green-300"} />
-                ))
-            }
-            <p>What you need to improve:</p>
-            {
-               worsen && worsen.map((text, index) => (
-                    <ImprovementCard key={index} text={text} color={"bg-red-300"}/>
-                ))
-            }
+
+            <List
+                className='my-3 bg-green-200'
+                bordered
+                dataSource={improved}
+                renderItem={(item) => (
+                    <List.Item className='flex'>
+                        <p className='mr-10'>{item}</p>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="absolute w-5 h-5 right-4">
+                            <path d="M1 8.25a1.25 1.25 0 1 1 2.5 0v7.5a1.25 1.25 0 1 1-2.5 0v-7.5ZM11 3V1.7c0-.268.14-.526.395-.607A2 2 0 0 1 14 3c0 .995-.182 1.948-.514 2.826-.204.54.166 1.174.744 1.174h2.52c1.243 0 2.261 1.01 2.146 2.247a23.864 23.864 0 0 1-1.341 5.974C17.153 16.323 16.072 17 14.9 17h-3.192a3 3 0 0 1-1.341-.317l-2.734-1.366A3 3 0 0 0 6.292 15H5V8h.963c.685 0 1.258-.483 1.612-1.068a4.011 4.011 0 0 1 2.166-1.73c.432-.143.853-.386 1.011-.814.16-.432.248-.9.248-1.388Z" />
+                        </svg>
+
+                    </List.Item>
+                )}
+            />
+            <p>What did you worsen:</p>
+
+            <List
+                locale={localeWorsen}
+                className='my-3 '
+                bordered
+                dataSource={worsen}
+                renderItem={(item) => (
+                    <List.Item>
+                        {item}
+                    </List.Item>
+                )}
+            />
 
         </Modal>
     )
