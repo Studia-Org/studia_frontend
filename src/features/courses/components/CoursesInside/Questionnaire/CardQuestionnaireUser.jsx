d5dde6ba1ce0d15f9ee5a8e0a4def60ce23ceb10
rogeranyor <64661531+rogeranyor@users.noreply.github.com>
2024-08-06 11:51:49 +0200

    remove duplicated data

--- src/features/courses/components/CoursesInside/Questionnaire/CardQuestionnaireUser.jsx
+++ src/features/courses/components/CoursesInside/Questionnaire/CardQuestionnaireUser.jsx
@@ -22,31 +22,19 @@ export const CardQuestionnaireUser = ({ user, setQuestionnaireAnswerData }) => {
             whileHover={{ scale: 1.01 }}
         >
             <div className='flex items-center gap-2'>
-                <Avatar shape="square" size="large" src={user.attributes.user.data.attributes.profile_photo?.data?.attributes?.url} />
-                <p className='text-sm font-medium text-gray-600'>{user.attributes.user.data.attributes.name}</p>
+                <Avatar shape="square" size="large" src={user?.attributes?.user?.data?.attributes?.profile_photo?.data?.attributes?.url} />
+                <p className='text-sm font-medium text-gray-600'>{user?.attributes?.user?.data?.attributes?.name}</p>
             </div>
             <p className='ml-auto text-sm text-gray-600'> <strong> {t("QUESTIONNAIRE.completed_in")}:</strong> {user.attributes.timeToComplete}</p>
             <p className='ml-auto text-sm text-gray-600'><strong> {t("QUESTIONNAIRE.completation_date")}:</strong>  {formattedDate}</p>
             {
-                user.attributes.user.data?.attributes && (
-                    <>
-                        <div className='flex items-center gap-2'>
-                            <Avatar shape="square" size="large" src={user.attributes.user?.data?.attributes.profile_photo?.data?.attributes?.url} />
-                            <p className='text-sm font-medium text-gray-600'>{user.attributes.user.data.attributes.name}</p>
-                        </div>
-                        <p className='ml-auto text-sm text-gray-600'> <strong> Time to complete:</strong> {user.attributes.timeToComplete}</p>
-                        <p className='ml-auto text-sm text-gray-600'><strong>Completion date:</strong>  {formattedDate}</p>
-                        {
-                            user.attributes.qualification?.attributes?.qualification && (
-                                <div className='flex items-center justify-center h-full py-2 ml-auto bg-green-500 rounded-md w-9'>
-                                    <p className='text-sm font-medium text-white'>{user.attributes.qualification?.attributes?.qualification}</p>
-                                </div>
-                            )
-                        }
-                    </>
+                user.attributes.qualification?.attributes?.qualification && (
+                    <div className='flex items-center justify-center h-full py-2 ml-auto bg-green-500 rounded-md w-9'>
+                        <p className='text-sm font-medium text-white'>{user.attributes.qualification?.attributes?.qualification}</p>
+                    </div>
                 )
             }
-        </motion.div>
+        </motion.div >
     )
 
 }
